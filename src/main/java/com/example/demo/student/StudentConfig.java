dd537e2467b71ccc885632828d5d772c43cfec47
Roopam <roopamjain@catalyst.net.nz>
2022-08-13 19:16:10 +1200

    service and repository layer and enabling jpa

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.naming.Name;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student roopam = new Student(
+                1L,
+                "Roopam",
+                LocalDate.of(1990, 05, 22),
+                22,
+                "roopamjain01@gmail.com"
+            );
+
+            Student lovesh = new Student(
+                2L,
+                "Lovesh",
+                LocalDate.of(1990, 05, 21),
+                22,
+                "roopamjain01@gmail.com"
+            );
+
+            repository.saveAll(
+                List.of(roopam, lovesh)
+            );
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -7,16 +8,14 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> getStudent() {
-        return List.of(
-            new Student(
-                1L,
-                "Roopam Jain",
-                LocalDate.of(2000, 06, 22),
-                22,
-                "roopamjain01@gmail.com"
-            )
-        );
+        return studentRepository.findAll();
     }
 }
